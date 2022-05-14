a2d71fce06660ade9bdcc6e93e5e02f874b894e8
ZMB0Y <leo.zmby@gmail.com>
2022-05-13 19:07:53 -0500

    creacion de contador

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const result= document.getElementById("result")
+const suma= document.getElementById("suma")
+const resta= document.getElementById("resta")
+
+let count= 0
+
+suma.onclick = () => {
+    count+= 1
+    console.log(count)
+    result.innerHTML = count
+
+}
+
+resta.onclick=() =>{
+    if(count==0){
+        count=0
+    }
+    else{
+        count-= 1
+        console.log(count)
+        result.innerHTML=count
+    }
+    
+}
+
