b91a815945ce315fa7de36910de29f0b623277ea
ZMB0Y <leo.zmby@gmail.com>
2022-05-17 18:45:19 -0500

    Fundamentos de JS

---
+++ assets/js/fundamentals.js
@@ -0,0 +1,32 @@
+let number1=10
+let name="John"
+const b= true 
+/* console.log(name +" "+ number1)
+console.log(typeof(name))
+console.log(typeof(number1))
+console.log(typeof(b))
+console.log(typeof(name +" "+ number1 +" "+ b))
+console.log(typeof(number1+" "+b))
+console.log(number1+" "+b) */
+
+
+const person={
+    nombre: "leo",
+    apellido_paterno: "garcia",
+    apellido_materno:"hinojosa",
+    edad: "23",
+    email: "leo.zmby@",
+    telefono: "5566899030"
+}
+
+/* console.log(person.nombre +" "+ person.apellido_paterno +" "+ person.apellido_materno)
+console.log(`nombre completo: ${person.nombre} ${person.apellido_paterno} ${person.apellido_materno}`) */
+const { nombre, apellido_paterno, apellido_materno, telefono}= person
+// console.log(`Nombre Completo: ${nombre} ${apellido_paterno} ${apellido_materno} ${telefono}`)
+
+const videoGames= ["mario","infamous","uncharted","mariokart"] 
+console.log(videoGames[2])
+console.log(videoGames.length)
+
+const [a,e,c,d]= videoGames
+console.log(d)
