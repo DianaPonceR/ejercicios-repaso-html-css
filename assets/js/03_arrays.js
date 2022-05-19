d19b10600271b0322075398041191d5d496316bd
ZMB0Y <leo.zmby@gmail.com>
2022-05-19 13:34:48 -0500

    Ciclo FOR

---
+++ assets/js/03_arrays.js
@@ -0,0 +1,16 @@
+const frutas =["manzana", "platano chiapas", "pera", "uva"]
+console.log(frutas.length)
+console.log(frutas)
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+
+console.log()
+
+
+for(let index=0; index < frutas.length; index++) {
+    console.log((index+1) +".-" + frutas[index])
+    
+    //console.log(`${index+1} .- ${frutas[index]}`)
+
+}
