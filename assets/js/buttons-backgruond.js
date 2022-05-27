35b960d461900a408ea88e5b139f29fa4ad9b9e6
ZMB0Y <leo.zmby@gmail.com>
2022-05-26 19:12:43 -0500

    Boton de reinicio

--- assets/js/buttons-backgruond.js
+++ assets/js/buttons-backgruond.js
@@ -2,17 +2,20 @@ const boton1 = document.getElementById("boton1")
 const boton2 = document.getElementById("boton2")
 const boton3 = document.getElementById("boton3")
 const boton4 = document.getElementById("boton4")
+const boton5 = document.getElementById("boton5")
+const boton6 = document.getElementById("boton6")
+const boton7 = document.getElementById("boton7")
+const boton8 = document.getElementById("boton8")
+
+
+
 
 boton1.onclick = () => {
-    console.log("hola")
     const box1 = document.getElementById("box1") 
     box1.style.background = "red"
-    
-
 } 
 
 boton2.onclick = () => {
-    console.log("hola")
     const box2 = document.getElementById("box2") 
     box2.style.background = "green"
     
@@ -20,7 +23,6 @@ boton2.onclick = () => {
 } 
 
 boton3.onclick = () => {
-    console.log("hola")
     const box3 = document.getElementById("box3") 
     box3.style.background = "purple"
     
@@ -28,9 +30,28 @@ boton3.onclick = () => {
 } 
 
 boton4.onclick = () => {
-    console.log("hola")
     const box4 = document.getElementById("box4") 
     box4.style.background = "orange"
     
 
+} 
+
+boton5.onclick = () => {
+    const box1 = document.getElementById("box1") 
+    box1.style.background = "aliceblue"
+} 
+
+boton6.onclick = () => {
+    const box2 = document.getElementById("box2") 
+    box2.style.background = "aliceblue"
+} 
+
+boton7.onclick = () => {
+    const box3 = document.getElementById("box3") 
+    box3.style.background = "aliceblue"
+} 
+
+boton8.onclick = () => {
+    const box4 = document.getElementById("box4") 
+    box4.style.background = "aliceblue"
 } 
